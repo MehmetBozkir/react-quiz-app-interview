65c61f7573baea99827ab3015dddcb3198e20356
MehmetBozkir <mehmetsalihbozkir@gmail.com>
2024-11-04 17:52:11 +0300

    Added a question counter to keep track of how many questions you are on

--- src/app/components/pieces/QuestionCard.jsx
+++ src/app/components/pieces/QuestionCard.jsx
@@ -1,9 +1,18 @@
-const QuestionCard = ({ question, onAnswer, isClickable }) => {
+const QuestionCard = ({
+  question,
+  onAnswer,
+  isClickable,
+  currentQuestion,
+  questionslength,
+}) => {
   const choices = ["A", "B", "C", "D"];
 
   return (
     <div className="question-card p-4 md:w-3/5 mx-auto">
       <div className="mockup-window bg-gray-300 border">
+        <h1 className="bg-white text-end px-4 text-lg py-2 font-bold">
+          {currentQuestion + 1} / {questionslength}
+        </h1>
         <div className="bg-white justify-center px-4 py-16">
           <h2 className="question text-xl mb-20">{question.question}</h2>
           <div className="grid md:grid-cols-2 gap-4">
